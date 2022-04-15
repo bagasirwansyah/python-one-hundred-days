b1f0280a4047aa1d122a3af897236185fdfdba8e
Bagas Irwansyah <bagasirwansyah@yahoo.com>
2022-04-15 15:27:19 +0700

    Completed day 1/100

---
+++ day-1/learn.py
@@ -0,0 +1,29 @@
+""" This file is functioning as scrap paper """
+
+# print("Hello world!")
+
+
+# print("Hello world!\nHello World!")
+
+
+# print("Hello" + " " + "Angela")
+
+
+# input will be executed first
+# print("Hello " + input("What is your name?") + "!")
+
+
+# name = input("What is your name?")
+# print(name)
+
+
+# name = "Jack"
+# print(name)
+#
+# name = "Angela"
+# print(name)
+
+
+name = input("What is your name?")
+length = len(name)
+print(length)
---
+++ day-1/project.py
@@ -0,0 +1,7 @@
+""" Project of Day 1: Band Name Generator """
+
+print("Welcome to the Band Name Generator!")
+city = input("What is the name of the city you grew up in?\n")
+pet = input("What is the name of your pet?\n")
+
+print("I suggest you to use '{} {}' as your band name".format(city, pet))
