b4dcecdf02d30e538b5921980e750f558f80c99c
Bagas Irwansyah <bagasirwansyah@yahoo.com>
2022-04-18 14:19:56 +0700

    Completed day 2/100

---
+++ day-2/learn.py
@@ -0,0 +1,65 @@
+""" This file is functioning as scrap paper """
+
+# String
+
+# Subscripting, start with zero
+# print("Hello"[0])
+
+
+# print("123" + "345")
+
+
+
+# Integer
+
+# print(123 + 345)
+
+
+# print(123_456_789)
+
+
+
+# Float
+
+# print(3.14159)
+
+
+
+# Boolean
+
+# print(True)
+
+
+
+# num_char = len(input("What is your name?"))
+# print("Your name has " + str(num_char) + " characters.")
+# print(type(num_char))
+
+
+# a = float(123)
+# print(type(a))
+
+
+# print(3 + 5)
+# print(7 - 4)
+# print(3 * 2)
+# print(6 / 3)
+# print(2 ** 3  )
+# print(3 * 3 + 3 / 3 - 3)
+
+
+# print(round(8/3))
+# print(8//3)
+
+
+# score = 0
+# score += 1
+# print(score)
+
+
+score = 0
+height = 1.8
+isWinning = True
+
+print("Your score is " + str(score))
+print(f"Your score is {score}, your height is {height}, are you winning? {isWinning}")
---
+++ day-2/project.py
@@ -0,0 +1,10 @@
+""" Project of Day 1: Tip Calculator """
+
+total = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many people to split the bill? "))
+
+person = (total + (total * tip / 100)) / people
+person = round(person, 2)
+
+print(f"Each person should pay: ${person}")
